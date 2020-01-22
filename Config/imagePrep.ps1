85769a504ac9c6b0c8951c581bc78a7a6fd0b4c7
Dean Ralph <54666004+scootiemagee@users.noreply.github.com>
2020-01-22 13:51:13 +0000

    Update imagePrep.ps1

--- Config/imagePrep.ps1
+++ Config/imagePrep.ps1
@@ -1,3 +1,12 @@
+
+$wifiName =  Read-Host -Prompt 'Input your WiFi name'
+$wifiPass =  Read-Host -AsSecureString -Prompt 'Input your WiFi passkey'
 $piDrive = Get-Volume | where {$_.FileSystemLabel -like '*boot*'}
+$wifiFile = Get-Content wpa_supplicant.conf
 
 New-Item $piDrive.Path\ssh
+
+$wifiFile -Replace 'ssid="', 'ssid="$wifiName"'
+$wifiFile -Replace 'psk="', 'psk="$wifiPass"'
+
+Set-Content $piDrive.Path\wpa_supplicant.conf
