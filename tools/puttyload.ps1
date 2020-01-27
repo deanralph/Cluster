cb18557607f803d72368eda4fc67899d43482bce
Dean Ralph <54666004+scootiemagee@users.noreply.github.com>
2020-01-27 11:17:30 +0000

    Update puttyload.ps1

--- tools/puttyload.ps1
+++ tools/puttyload.ps1
@@ -2,5 +2,10 @@ $servers = Get-Content servercreds.txt
 
 foreach ($line in $servers){
     $splitLine = $line.Split(" ",[StringSplitOptions]'RemoveEmptyEntries')
-    putty.exe -ssh pi@$splitLine[0] -pw $splitLine[1]
+    if (Test-Connection $splitLine[0]){
+        putty.exe -ssh pi@$splitLine[0] -pw $splitLine[1]
+    }
+    Else{
+        echo "$splitLine[0] Offline"
+    }
 }
