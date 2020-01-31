bcd27e706b3d5bb91457490bbebd4d07a092df69
Dean Ralph <54666004+deanralph@users.noreply.github.com>
2020-01-31 09:10:15 +0000

    Update puttyload.ps1

--- tools/puttyload.ps1
+++ tools/puttyload.ps1
@@ -1,10 +1,11 @@
 $servers = Get-Content servercreds.txt
-$splitLine = $line.Split(" ",[StringSplitOptions]'RemoveEmptyEntries')
-$serverAdder = $splitLine[0]
-$serverPW = $splitLine[1]
 $os = Get-ChildItem -path env:OS
 
 foreach ($line in $servers){
+
+    $splitLine = $line.Split(" ",[StringSplitOptions]'RemoveEmptyEntries')
+    $serverAdder = $splitLine[0]
+    $serverPW = $splitLine[1]
     
     if (Test-Connection $serverAdder){
         if($os.Value -contains "Windows_NT"){
